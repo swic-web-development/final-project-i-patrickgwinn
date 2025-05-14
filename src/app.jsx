b8b4008cd765c8dc139510caf854c440358fd1c2
Patrick Gwinn <patrick.gwinn@student.swic.edu>
2025-05-14 10:33:10 -0500

    add data fetch statements

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,21 @@
+import React, { useState, useEffect } from 'react'
+
 export default function App() {
-  return (
-    <main>
-      <h1 className="text-3xl text-red-500">Hello, World!</h1>
-      <p>This is a simple React application.</p>
-    </main>
-  )
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    async function fetchPeople() {
+      const res = await fetch('https://swapi.tech/api/people/?format=json')
+      const data = await res.json()
+      setPeople(data.results)
+    }
+    fetchPeople()
+      .then(() => setLoading(false))
+      .catch((error) => {
+        setError(error)
+        setLoading(false)
+      })
+  }, [])
 }
